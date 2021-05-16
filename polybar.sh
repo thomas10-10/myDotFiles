5da4000d48cd4e23e406e2bac9fae5395e1e8c0e
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2021-05-16 22:48:02 +0200

    Create polybar.sh

---
+++ polybar.sh
@@ -0,0 +1,13 @@
+# Terminate already running bar instances
+killall -q polybar
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Launch bar1 and bar2
+polybar example &
+echo "Bars launched..."
+killall -q compton
+#compton -c -r 4 -l -2 -t -4 -G -f -b
+compton -f
+#feh --bg-scale ~root/wall/213493.png 
+#xrdb ~/.Xresources
+#wal -i ~/.wallpapers/wolf.jpg
