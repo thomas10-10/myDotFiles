3b6f16e481e77e4ba24ffff2c3af8a573bf9c915
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2021-05-17 01:24:38 +0200

    Update polybar.sh

--- polybar.sh
+++ polybar.sh
@@ -6,8 +6,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar example &
 echo "Bars launched..."
 killall -q compton
-#compton -c -r 4 -l -2 -t -4 -G -f -b
-compton -f
+compton -c -r 4 -l -4 -t -4 -G -f -b
+#compton -f
 #feh --bg-scale ~root/wall/213493.png 
 #xrdb ~/.Xresources
 #wal -i ~/.wallpapers/wolf.jpg
